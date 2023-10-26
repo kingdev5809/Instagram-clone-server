34e93d46069cc59d90fc11b72eb6336566090fe4
KING <kingdev5809@gmail.com>
2023-10-26 14:57:53 +0500

    awdawd

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 // xFu3dCNcGALmLdyG
 mongoose
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const {
   register,
   getAllUsers,
   logOut,
-  getOfflineUsers
+  getOfflineUsers,
 } = require("../controllers/userController");
 
 const router = require("express").Router();
@@ -12,5 +12,5 @@ router.post("/login", login);
 router.post("/register", register);
 router.get("/allusers/:id", getAllUsers);
 router.get("/logout/:id", logOut);
-router.get('/getofflineUsers', getOfflineUsers)
+router.get("/getofflineUsers", getOfflineUsers);
 module.exports = router;
